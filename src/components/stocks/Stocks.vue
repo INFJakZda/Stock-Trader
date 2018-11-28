3491021bee5c708e31ef047d86e4e647be6d3793
INFJakZda <zdanowski78@gmail.com>
2018-11-28 18:15:32 +0100

    Add stocks & stock template

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,13 +1,31 @@
 <template>
-    <h1>The Stocks Component</h1>
+  <div>
+    <app-stock 
+      v-for="stock in stocks"
+      :key="stock.id">
+    </app-stock>
+  </div>
 </template>
 
 <script>
-export default {
+import Stock from './Stock.vue';
 
-}
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 10 },
+        { id: 1, name: "Google", price: 132 },
+        { id: 1, name: "Tinder", price: 211 },
+        { id: 1, name: "Yahoo", price: 41 }
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
+};
 </script>
 
 <style>
-
 </style>
