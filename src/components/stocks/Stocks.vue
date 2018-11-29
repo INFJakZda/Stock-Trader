5d09dd33e30f7e4f01939c75eca4107a55c31e78
INFJakZda <zdanowski78@gmail.com>
2018-11-29 21:42:11 +0100

    Add vuex & Stocks store / state

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -12,18 +12,13 @@
 import Stock from './Stock.vue';
 
 export default {
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: "BMW", price: 10 },
-        { id: 2, name: "Google", price: 132 },
-        { id: 3, name: "Tinder", price: 211 },
-        { id: 4, name: "Yahoo", price: 41 }
-      ]
-    }
-  },
   components: {
     appStock: Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks;
+    }
   }
 };
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: "BMW", price: 10 },
+  { id: 2, name: "Google", price: 132 },
+  { id: 3, name: "Tinder", price: 211 },
+  { id: 4, name: "Yahoo", price: 41 }
+];
